e9c2bd7279fc28694a2efb706c4d8a5e61182aee
winifredogbeiwi <imadeogbeiwi@gmail.com>
2025-02-25 11:38:43 +0100

    working around basic java debugging

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.text.MessageFormat;
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
+
+        //Testing debugging
+        // System.out.println(3/0);
+       try {
+           System.out.println(3/0);
+       } catch (Exception e) {
+           System.out.println(e.getMessage());
+       }
     }
+
+
+
 }
